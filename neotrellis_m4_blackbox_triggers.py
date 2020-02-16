9eee152503ee89f7226956886f753acef73ce7a5
John Park <jp@jpixl.net>
2020-02-16 09:19:50 -0800

    Code optimization for redundant state checks on non-toggle pads.

--- neotrellis_m4_blackbox_triggers.py
+++ neotrellis_m4_blackbox_triggers.py
@@ -1,7 +1,7 @@
 # NeoTrellis M4 8x4 Blackbox Trigger Samples/Sequences
 # John Park
-# February 15, 2020
-# version 0.2
+# February 16, 2020
+# version 0.3
 
 import time
 import adafruit_trellism4
@@ -27,7 +27,7 @@ ORANGE = (30, 20, 0)
 MAGENTA = (15, 0, 30)
 PINK = (20, 0, 15)
 AQUA = (0, 8, 18)
-#WHITE = (5, 5, 6)
+# WHITE = (5, 5, 6)
 
 COLOR_A = AQUA
 COLOR_B = PINK
@@ -36,12 +36,12 @@ COLOR_D = CYAN
 
 for x in range(4):
     for y in range(4):
-        trellis.pixels[x, y]=COLOR_B
+        trellis.pixels[x, y] = COLOR_B
         time.sleep(0.02)
 
 for x in range(4, 8):
     for y in range(4):
-        trellis.pixels[x, y]=COLOR_D
+        trellis.pixels[x, y] = COLOR_D
         time.sleep(0.02)
 
 current_press = set()
@@ -52,8 +52,9 @@ midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=midi_channel-1
 
 
 # button mappings into logical grids of MIDI notes for blackbox
+
 note_map = {
-    (0,3): (36), # left 16 corresond to blackbox PADS sample cells
+    (0,3): (36),  # left 16 corresond to blackbox PADS sample cells
     (1,3): (37),
     (2,3): (38),
     (3,3): (39),
@@ -74,7 +75,7 @@ note_map = {
     (3,0): (51),
 
 
-    (4,3): (52), # right 16 correspond to blackbox SEQS sequence launch cells
+    (4,3): (52),  # right 16 correspond to blackbox SEQS sequence launch cells
     (5,3): (53),
     (6,3): (54),
     (7,3): (55),
@@ -97,38 +98,40 @@ note_map = {
 
 while True:
     pressed = set(trellis.pressed_keys)  # a button is pressed
+
     for press in pressed - current_press:  # press down event
         x, y = press  # get the address of the pressed button
         print(" ")
         print("Pressed: ", press)
+
+        # Send MIDI Note On message
         if press in note_map:  # send MIDI message
             midi.send(NoteOn(note_map[press], 64))  # midi on, velocity 64
             print("MIDI On: ", note_map[press])
 
-        if not led_on[x][y]:  # check for toggle state (really only matters for sequences)
-            if x < 4:  # left pads don't bother with toggling
-                print("Pad LED press blue:", press)
-                trellis.pixels[x, y] = COLOR_A
-            else:
+        # Simple color change for momentary pads
+        if x < 4:  # left pads
+            trellis.pixels[x, y] = COLOR_A
+            print("Pad LED press blue:", press)
+
+        # Change the color & toggle state of sequence buttons
+        else:
+            if not led_on[x][y]:
                 print("Toggle Sequence LED on:", press)
                 trellis.pixels[x, y] = COLOR_C
-            led_on[x][y] = True
-
-        else:
-            if x < 4:  # left pads don't bother with toggling
-                print("Pad LED press blue:", press)
-                trellis.pixels[x, y] = COLOR_A
+                led_on[x][y] = True
             else:
                 print("Toggle Sequence LED off:", press)
                 trellis.pixels[x, y] = COLOR_D
-            led_on[x][y] = False
+                led_on[x][y] = False
+
 
     for release in current_press - pressed:  # release up event
         x, y = release  # get the address of the released button
         print(" ")
         print("Released: ", release)
         if release in note_map:
-            if x < 4: # only send message for Pads, not sequences
+            if x < 4:  # only send message for Pads, not sequences
                 trellis.pixels[x, y] = COLOR_B
                 midi.send(NoteOff(note_map[release], 0))  # midi off, velocity 0
                 print("MIDI Off: ", note_map[release])
